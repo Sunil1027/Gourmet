12c5bce20b85868d3a964369e3b9f1e0b0687ea4
Thapa Sunil <thapa.saroj847@gmail.com>
2026-06-22 14:16:00 +0900

    初期設定

---
+++ src/main/java/com/example/gourmetManagementApp/GourmetManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.gourmetManagementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GourmetManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GourmetManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gourmetManagementApp/GourmetManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gourmetManagementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GourmetManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
