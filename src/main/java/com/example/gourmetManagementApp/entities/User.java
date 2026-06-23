ad1b1c0c751d8ab94037e73dff0438f000c286ae
NS <shunsuke.kenshuu@gmail.com>
2026-06-23 16:19:29 +0900

    セキュリティ

--- src/main/java/com/example/gourmetManagementApp/entities/User.java
+++ src/main/java/com/example/gourmetManagementApp/entities/User.java
@@ -1,5 +1,5 @@
 package com.example.gourmetManagementApp.entities;
 
-public class User {
+	public class User {
 
 }
--- src/main/java/com/example/gourmetManagementApp/reposities/UserRepository.java
+++ src/main/java/com/example/gourmetManagementApp/reposities/UserRepository.java
@@ -1,5 +1,18 @@
 package com.example.gourmetManagementApp.reposities;
 
-public class UserRepository {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.gourmetManagementApp.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	// 主キー検索
+	Optional<User> findById(long id);
+	// ユーザー名(userId)検索
+	List<User> findByUserIdContaining(String userId);
+	Optional<User> findByUserId(String userId);
+	// ユーザー削除
+	User deleteById(long id);
 }
