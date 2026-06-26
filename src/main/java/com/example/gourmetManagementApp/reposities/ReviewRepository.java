c5ec045da38cd7bc856dc5d44977167f71b538f9
NS <shunsuke.kenshuu@gmail.com>
2026-06-26 10:52:50 +0900

    修正

--- src/main/java/com/example/gourmetManagementApp/reposities/ReviewRepository.java
+++ src/main/java/com/example/gourmetManagementApp/reposities/ReviewRepository.java
@@ -14,7 +14,8 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 	Optional<Review> findById(long id);
 	
 	void deleteByRestaurant_Id(Long restaurantId);
-//	List<Review> findByRestaurant_Id(Long id);
+
+
 	 List<Review> findByUserId(String userId);
 
 	 void deleteByRestaurantId(long id);
